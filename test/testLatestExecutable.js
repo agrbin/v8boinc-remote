df9956f492c122be95d147beaa06c53dd4adba6b
Anton Grbin <anton.grbin@fer.hr>
2014-08-20 20:15:40 +0200

    get latest executable backend

---
+++ test/testLatestExecutable.js
@@ -0,0 +1,23 @@
+var di = require('../lib/DI.js'),
+  test = require('./test.js');
+
+var V8Boinc = require('./..'),
+  v8b = new V8Boinc(
+    {
+      project : 'http://v8boinc.fer.hr/v8boinc/',
+      authenticator : test.getAuth()
+    },
+    test.getSniffer('tmp')
+  );
+
+v8b.initialize(function (err) {
+  test.expectFalse(err);
+
+  v8b.getCodeSignPublicKey(function (err, pubkey) {
+
+    v8b.getLatestExecutable('i686-pc-linux-gnu', function (err, data) {
+      console.log(err,  data );
+    });
+  });
+
+});
